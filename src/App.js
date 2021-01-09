844f95d5cff2a9cb49bb319440ed5f1538c213db
Sara <sarakirving@gmail.com>
2021-01-09 13:23:32 -0800

    import usePokemonData hook into main app file to make sure that state etc are accessible here

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import usePokemonData from './hooks/usePokemonData';
 
 function App() {
+  const {state, setState, getPokemon} = usePokemonData();
+
+  console.log("state = ", state);
+  
+  // need to think about a way we can grab the input from the textarea and feed it to the getPokemon function when we click the button? probably by interacting with the DOM? 
+
   return (
     <div className="App">
      Welcome to the Pokedex!
+     <Button buttonLabel="Find that Pokemon!"></Button>
     </div>
   );
 }
