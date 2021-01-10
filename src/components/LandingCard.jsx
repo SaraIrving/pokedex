f6c08bff0600d4a4bbdd7084b5ea37f2cbaffca2
Sara <sarakirving@gmail.com>
2021-01-10 12:16:44 -0800

    refactor so that LandingCard renders the new InfoStats component for each of the three randomly chosed pokemon to display initially

--- src/components/LandingCard.jsx
+++ src/components/LandingCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import InfoStats from './InfoStats';
 //import usePokemonData from '../hooks/usePokemonData';
 
 
@@ -15,23 +16,7 @@ export default function LandingCard(props) {
       {console.log("in Landing Card, randomPokemon = ", props.state.randomPokemon)}
       {props.state.randomPokemon.map((random, index) => {
         return (
-          <div>
-            <p>name = {random.name}</p>
-            <p>height = {random.hight}</p>
-            <p>weight = {random.weight}</p>
-            <p>types = {random.types.map((type, index) => {
-              return (
-                <p>{type.type.name}</p>
-              );
-            })}</p>
-            <p>abilities = {random.abilities.map((ableTo, index) => {
-              return (
-                <p>{ableTo.ability.name}</p>
-              );
-            })}</p>
-            <p>base stats</p>
-            
-          </div>
+          <InfoStats random={random}></InfoStats>
         )
       })}
       <Button buttonLabel="Click here to start your search!" onClick={() => (props.setState(prev => ({...prev, randomPokemon: [], view: "search"})))}></Button>
