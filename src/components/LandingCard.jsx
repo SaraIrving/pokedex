9a30669f6c6514aade0399799d6f889e6cabcad9
Sara <sarakirving@gmail.com>
2021-01-09 22:46:03 -0800

    update return so that is maps over the randomPokemon array in state and displays the info, hardcoded for now but eventually it will return an card component

--- src/components/LandingCard.jsx
+++ src/components/LandingCard.jsx
@@ -13,6 +13,13 @@ export default function LandingCard(props) {
     <div>
       <p>This is the initial landing card with random pokemon to display</p>
       {console.log("in Landing Card, randomPokemon = ", props.state.randomPokemon)}
+      {props.state.randomPokemon.map((random, index) => {
+        return (
+          <div>
+            <p>{random.name}</p>
+          </div>
+        )
+      })}
       <Button buttonLabel="Click here to start your search!" onClick={() => (props.setState(prev => ({...prev, randomPokemon: []})))}></Button>
 
     </div>
