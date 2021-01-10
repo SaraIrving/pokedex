7dae1137b5c8ebb996d28e3d5599d65d7684bfed
Sara <sarakirving@gmail.com>
2021-01-09 20:02:12 -0800

    refactor to bring in the clearRandom function in an attempt to clear the randomPokemon object in the state upon click of the button, so far it is not working as desired, does not seem to be updating the state

--- src/components/LandingCard.jsx
+++ src/components/LandingCard.jsx
@@ -5,14 +5,14 @@ import usePokemonData from '../hooks/usePokemonData';
 
 export default function LandingCard(props) {
 
-  const {state, setState, getPokemon} = usePokemonData();
+  const {state, setState, getPokemon, clearRandom} = usePokemonData();
 
 
 
   return (
     <div>
       <p>This is the initial landing card with random pokemon to display</p>
-      <Button buttonLabel="Click here to start your search!" onClick={() => {setState(prev => ({...prev, randomPokemon:{}}))}}></Button>
+      <Button buttonLabel="Click here to start your search!" onClick={clearRandom}></Button>
 
     </div>
     
