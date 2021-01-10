fb091ea35e19e3a90750fa036494f2969edc8d1b
Sara <sarakirving@gmail.com>
2021-01-09 22:07:12 -0800

    refactor to use the prop setState instead of importing in setState from the usePokemonData file, unsure if this is a permenant fix or not but got the app working for now

--- src/components/LandingCard.jsx
+++ src/components/LandingCard.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import Button from './Button';
-import usePokemonData from '../hooks/usePokemonData';
+//import usePokemonData from '../hooks/usePokemonData';
 
 
 export default function LandingCard(props) {
 
-  const {state, setState, getPokemon, clearRandom} = usePokemonData();
+  //const {state, setState, getPokemon, clearRandom} = usePokemonData();
 
 
 
   return (
     <div>
       <p>This is the initial landing card with random pokemon to display</p>
-      <Button buttonLabel="Click here to start your search!" onClick={clearRandom}></Button>
+      <Button buttonLabel="Click here to start your search!" onClick={() => (props.setState(prev => ({...prev, randomPokemon: {}})))}></Button>
 
     </div>
     
-
 
   )
 
