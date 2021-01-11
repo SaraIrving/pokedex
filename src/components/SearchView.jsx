6be74535a40417e1e029091e084e8e47283ed249
Sara <sarakirving@gmail.com>
2021-01-11 14:14:28 -0800

    comment out unsued elements, not currently using button anymore

--- src/components/SearchView.jsx
+++ src/components/SearchView.jsx
@@ -9,9 +9,9 @@ export default function SearchView (props) {
   return (
     <div>
       <SearchBox state={props.state} setState={props.setState}></SearchBox>
-      <Button buttonLabel="Find that Pokemon!" onClick={props.onClick}></Button>
+      {/* <Button buttonLabel="Find that Pokemon!" onClick={props.onClick}></Button>
       <p>{props.state.searchedPokemon.name}</p>
-      <p>RENDER SEARCHED CARD HERE</p>
+      <p>RENDER SEARCHED CARD HERE</p> */}
       {props.state.searchedPokemon.name && <InfoStats pokemon={props.state.searchedPokemon} color={props.state.searchedColor}></InfoStats>}
     </div>
   )
