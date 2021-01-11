3bb7cd7f49535306f5e4db6022be3bb7900ecaba
Sara <sarakirving@gmail.com>
2021-01-11 12:22:48 -0800

    import in SearchBox component

--- src/components/SearchView.jsx
+++ src/components/SearchView.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Button from './Button';
+import SearchBox from './SearchBox';
 
 export default function SearchView (props) {
-  console.log("SEARCH props = ", props)
+  //console.log("SEARCH props = ", props)
 
   return (
     <div>
-      <p>{props.state.searchedPokemon.name}</p>
+      <SearchBox></SearchBox>
       <Button buttonLabel="Find that Pokemon!" onClick={props.onClick}></Button>
+      <p>{props.state.searchedPokemon.name}</p>
     </div>
   )
 
