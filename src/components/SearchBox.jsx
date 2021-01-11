9b68534182b47cb2e4f0e91ab500ef60af24f7f2
Sara <sarakirving@gmail.com>
2021-01-11 12:20:39 -0800

    create component with autocomplete search area leveraging components borrowed from Material UI, need to work out how to gain access to the selected value so it can be used to make the api call, have currently isolated value, now need to experiment with using that value to update the state and trigger an api call

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+const pokemon = require('pokemon');
+
+
+export default function SearchBox() {
+
+  const pokemonOptions = pokemon.all();
+  console.log("ALL POKEMON == ", pokemonOptions);
+
+
+
+  return (
+    <form>
+        <Autocomplete
+        onChange={(event, value) => console.log("CHANGE VALUE = ", value)}
+      id="searchBox"
+      options={pokemonOptions}
+      getOptionLabel={(option) => option}
+      style={{ width: 300 }}
+      renderInput={(params) => {return (<TextField {...params} label="Pokemon name..." variant="outlined" />)}}
+    />
+    </form>
+  
+    
+  );
+}
+
