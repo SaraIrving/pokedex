6219e3b05e83cdf3f513319f97b2ceba4d0587a9
Sara <sarakirving@gmail.com>
2021-01-11 13:40:56 -0800

    refactor and change props variable names to be more generic so they work when we are rending the searched pokemon as well

--- src/components/InfoStats.jsx
+++ src/components/InfoStats.jsx
@@ -6,7 +6,7 @@ export default function InfoStats(props) {
   const style = {
                   borderWidth: "5px",
                   borderStyle: "solid",
-                  borderColor: props.randomColor
+                  borderColor: props.color
                 };
 
   return (
@@ -14,15 +14,15 @@ export default function InfoStats(props) {
       <div className="InfoStatsWrapper">
         <div>
           <h3>Basic Info</h3>
-          <p>name = {props.random.name}</p>
-          <p>height = {props.random.height}</p>
-          <p>weight = {props.random.weight}</p>
-          <p>types = {props.random.types.map((type, index) => {
+          <p>name = {props.pokemon.name}</p>
+          <p>height = {props.pokemon.height}</p>
+          <p>weight = {props.pokemon.weight}</p>
+          <p>types = {props.pokemon.types.map((type, index) => {
             return (
               <span key={index}>{type.type.name} </span>
             );
           })}</p>
-          <p>abilities = {props.random.abilities.map((ableTo, index) => {
+          <p>abilities = {props.pokemon.abilities.map((ableTo, index) => {
             return (
               <span key={index}>{ableTo.ability.name} </span>
             );
@@ -30,12 +30,12 @@ export default function InfoStats(props) {
         </div>
         <div>
           <h3>Base Stats</h3>
-          <p>hp: {props.random.baseStats[0].base_stat}</p>
-          <p>attack: {props.random.baseStats[1].base_stat}</p>
-          <p>defense: {props.random.baseStats[2].base_stat}</p>
-          <p>special-attack: {props.random.baseStats[3].base_stat}</p>
-          <p>special-defense: {props.random.baseStats[4].base_stat}</p>
-          <p>speed: {props.random.baseStats[5].base_stat}</p>
+          <p>hp: {props.pokemon.baseStats[0].base_stat}</p>
+          <p>attack: {props.pokemon.baseStats[1].base_stat}</p>
+          <p>defense: {props.pokemon.baseStats[2].base_stat}</p>
+          <p>special-attack: {props.pokemon.baseStats[3].base_stat}</p>
+          <p>special-defense: {props.pokemon.baseStats[4].base_stat}</p>
+          <p>speed: {props.pokemon.baseStats[5].base_stat}</p>
         </div>
       </div>
       
