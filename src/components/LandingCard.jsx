d39294282590f5187832c933267cacb14fe364a6
Sara <sarakirving@gmail.com>
2021-01-10 17:43:18 -0800

    use react reveal LightSpeed animation on the random pokemon cards being rendered

--- src/components/LandingCard.jsx
+++ src/components/LandingCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LightSpeed from 'react-reveal/LightSpeed';
 import Button from './Button';
 import InfoStats from './InfoStats';
 //import usePokemonData from '../hooks/usePokemonData';
@@ -17,11 +18,13 @@ export default function LandingCard(props) {
       {props.state.randomPokemon.map((random, index) => {
         console.log("INDEX = ", index)
         return (
-          <InfoStats random={random} randomColor={props.state.randomColorArray[index]}></InfoStats>
+          <LightSpeed>
+            <InfoStats random={random} randomColor={props.state.randomColorArray[index]}></InfoStats>
+          </LightSpeed>
+          
         )
       })}
       <Button buttonLabel="Click here to start your search!" onClick={() => (props.setState(prev => ({...prev, randomPokemon: [], view: "search"})))}></Button>
-
     </div>
     
 
