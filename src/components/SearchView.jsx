49073b6e93340be48cb89eb4798064e564fe0958
Sara <sarakirving@gmail.com>
2021-01-11 13:40:14 -0800

    pass the searched pokemon object from state and the searchedColor from state as props to the InfoStats component

--- src/components/SearchView.jsx
+++ src/components/SearchView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from './Button';
 import SearchBox from './SearchBox';
+import InfoStats from './InfoStats';
 
 export default function SearchView (props) {
   //console.log("SEARCH props = ", props)
@@ -11,6 +12,7 @@ export default function SearchView (props) {
       <Button buttonLabel="Find that Pokemon!" onClick={props.onClick}></Button>
       <p>{props.state.searchedPokemon.name}</p>
       <p>RENDER SEARCHED CARD HERE</p>
+      {props.state.searchedPokemon.name && <InfoStats pokemon={props.state.searchedPokemon} color={props.state.searchedColor}></InfoStats>}
     </div>
   )
 
