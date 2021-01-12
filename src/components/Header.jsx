a5ea148ad39a0b99f9e2d1a6ce9a6d700e9c2ee2
Sara <sarakirving@gmail.com>
2021-01-12 10:17:27 -0800

    refactor href on the home button to work when app is deployed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return(
     <div className="header">
       <img src={'https://www.logolynx.com/images/logolynx/s_84/84058ccd07dbf5a0389be9d79affd359.png'}></img>
-      <Button className="headerButton" onClick={event => window.location.href='/'} buttonLabel="Home"></Button>
+      <Button className="headerButton" onClick={event => window.location.href='/pokedex/'} buttonLabel="Home"></Button>
     </div>
   );
 }
