0c98c8b607dfd4019d0a3f1517d1fd9942568226
Sara <sarakirving@gmail.com>
2021-01-12 09:57:56 -0800

    add a home button in the header to make the app easier to navigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from './Button';
 
 export default function Header() {
 
   return(
     <div className="header">
       <img src={'https://www.logolynx.com/images/logolynx/s_84/84058ccd07dbf5a0389be9d79affd359.png'}></img>
+      <Button className="headerButton" onClick={event => window.location.href='/'} buttonLabel="Home"></Button>
     </div>
   );
 }
