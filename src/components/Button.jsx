a5c9cd222585d695f771304927f156d62db778e1
Sara <sarakirving@gmail.com>
2021-01-12 09:57:34 -0800

    refactor to accept a className as a prop so that the two buttons in the app can be styled differently

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export default function Button(props) {
  
   return (
-    <button type="submit" onClick={props.onClick} >{props.buttonLabel}</button>  
+    <button className={props.className} type="button" onClick={props.onClick} >{props.buttonLabel}</button>  
   );
 }
