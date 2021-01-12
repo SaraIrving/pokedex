246e83c4ff194a7076801883a2029858d64ab255
Sara <sarakirving@gmail.com>
2021-01-11 23:27:43 -0800

    no longer need to import or pass getPokemon function as a prop to SearchView component as we are not using it anymore

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import Header from './components/Header';
 import LandingCard from './components/LandingCard';
 import Loading from './components/Loading';
-import SearchBox from './components/SearchBox';
 import SearchView from './components/SearchView';
 import usePokemonData from './hooks/usePokemonData';
 
 
 function App() {
-  const {state, setState, getPokemon} = usePokemonData();
-
-  console.log("state = ", state);
+  const { state, setState } = usePokemonData();
   
-  // need to think about a way we can grab the input from the textarea and feed it to the getPokemon function when we click the button? probably by interacting with the DOM? 
-
+  //conditionally render components based on the view in state
+  
   return (
     <div className="App">
       <Header></Header>
@@ -21,7 +18,7 @@ function App() {
       {console.log('random length  = ', state.randomPokemon.length)}
       {state.view === "loading" && <Loading></Loading>}
       {state.view === "landing" && <LandingCard state={state} setState={setState}></LandingCard>}
-      {state.view === "search" &&  <SearchView state={state} setState={setState} onClick={getPokemon}></SearchView>}
+      {state.view === "search" &&  <SearchView state={state} setState={setState}></SearchView>}
       <footer></footer>
     </div>
   );
