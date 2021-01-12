6f69bfd9e4d8931430a1a21341bec58ea8df638e
Sara <sarakirving@gmail.com>
2021-01-12 09:18:18 -0800

    remove console.logs

--- src/App.js
+++ src/App.js
@@ -10,12 +10,10 @@ function App() {
   const { state, setState } = usePokemonData();
   
   //conditionally render components based on the view in state
-  
+
   return (
     <div className="App">
       <Header></Header>
-      {console.log('state =', state.randomPokemon)}
-      {console.log('random length  = ', state.randomPokemon.length)}
       {state.view === "loading" && <Loading></Loading>}
       {state.view === "landing" && <LandingCard state={state} setState={setState}></LandingCard>}
       {state.view === "search" &&  <SearchView state={state} setState={setState}></SearchView>}
