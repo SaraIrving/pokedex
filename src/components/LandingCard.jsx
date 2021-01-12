bbae7e40875a4a6c849f5a032247e6b9e206d5d0
Sara <sarakirving@gmail.com>
2021-01-11 17:54:39 -0800

    add some more html structure and classnames for styling purposes

--- src/components/LandingCard.jsx
+++ src/components/LandingCard.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import LightSpeed from 'react-reveal/LightSpeed';
 import Button from './Button';
 import InfoStats from './InfoStats';
-//import usePokemonData from '../hooks/usePokemonData';
 
 
 export default function LandingCard(props) {
 
-  //const {state, setState, getPokemon, clearRandom} = usePokemonData();
-
-
-
   return (
-    <div>
-      <p>Want to find your favorite pokemon? Click the button below to start your search!</p>
-      {/* {console.log("in Landing Card, randomPokemon = ", props.state.randomPokemon)} */}
+    <div className="landingView">
+      <div className="landingText">
+        <p>Want to find your favorite pokemon?</p>
+        <p>Click the button below to start your search!</p>
+      </div>
+      
       <Button buttonLabel="Click here to start your search!" onClick={() => (props.setState(prev => ({...prev, randomPokemon: [], view: "search"})))}></Button>
       <ul className="randomPokemonList">
         {props.state.randomPokemon.map((random, index) => {
